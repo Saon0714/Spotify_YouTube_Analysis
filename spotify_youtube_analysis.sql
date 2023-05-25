109cfcb560089d911e0108ed67213aaef3f84802
Saon Paul <112460162+Saon0714@users.noreply.github.com>
2023-05-25 12:28:10 +0530

    Update spotify_youtube_analysis.sql

--- spotify_youtube_analysis.sql
+++ spotify_youtube_analysis.sql
@@ -8,20 +8,25 @@ order by Views desc limit 1;
 select Track , Stream from cleaned_dataset
 order by Stream desc limit 1;
 
--- EnergyLiveness ratio is one of the popular ways to measure the quality of the song, which are the top 5 songs that have the highest energyliveness ratio.
+-- EnergyLiveness ratio is one of the popular ways to measure the quality of the song,
+-- which are the top 5 songs that have the highest energyliveness ratio.
 
 select Track, Energyliveness 
 from cleaned_dataset 
 order by Energyliveness desc limit 5;
 
--- let us assume a situation where an artist named Black Eyed Peas wants to analyze his songs. The artist wants to know which platform is capable of keeping his song track more engaged. To check this he assigns you this task and wants you to report to him where his song tracks are more played on. compare the platforms.
+-- let us assume a situation where an artist named Black Eyed Peas wants to analyze his songs.
+-- The artist wants to know which platform is capable of keeping his song track more engaged.
+-- To check this he assigns you this task and wants you to report to him
+-- where his song tracks are more played on. compare the platforms.
 
 select count(*),most_playedon
 from cleaned_dataset
 where Artist = "Black Eyed Peas"
 group by most_playedon;
 
--- Gorillaz wants to know their most liked song on youtube. Report to them with their most liked song along with the Energy and Tempo of the song.
+-- Gorillaz wants to know their most liked song on youtube.
+-- Report to them with their most liked song along with the Energy and Tempo of the song.
 
 select Track,Likes, Energy, Tempo
 from cleaned_dataset
@@ -34,7 +39,8 @@ select Album_type, count(Album_type)
 from cleaned_dataset 
 group by Album_type;
 
--- Spotify's most loved song tracks are to be declared soon. Help Spotify choose the top 5 most streamed+youtube viewed song track.
+-- Spotify's most loved song tracks are to be declared soon.
+-- Help Spotify choose the top 5 most streamed+youtube viewed song track.
 
 select distinct(Track),Stream+Views as Total
 from cleaned_dataset
